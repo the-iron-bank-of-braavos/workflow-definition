0241a97505f388254dd809c4c19040fb83e7ad6f
David Hernández Martín <david.hernandezmartin@gruposantander.com>
2021-07-12 12:53:47 +0200

    Update App.java (#1)
    Creating files for workflows

--- src/main/java/com/theironbank/App.java
+++ src/main/java/com/theironbank/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\nApplication successfully executed\n" );
+        System.out.println( "\n\tApplication successfully executed\n" );
     }
 }
