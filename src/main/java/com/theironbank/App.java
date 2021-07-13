0eb7b72e17706263eabb7d2d2a3bb9c99ff71aaa
David Hernández Martín <david.hernandezmartin@gruposantander.com>
2021-07-13 12:29:40 +0200

    Develop (#15)
    * Update App.java (#13)
    
    sadasdasd
    
    * Update AppTest.java (#14)
    
    * Update AppTest.java
    
    * Update AppTest.java

--- src/main/java/com/theironbank/App.java
+++ src/main/java/com/theironbank/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\n\tApplication successfully executed\n" );
+        System.out.println( "--- Application successfully executed\n" );
     }
 }
